d9fca5c1fad020e0e236507fab129710818f4c5f
Mike Kivikoski <mkivikoski@cantina.co>
2017-10-24 15:40:19 -0400

    sidebar: add Cantina as site partner in sidebar

--- wp-content/themes/artprof/sidebar-artprof-sidebar.php
+++ wp-content/themes/artprof/sidebar-artprof-sidebar.php
@@ -3,9 +3,9 @@
     <div class="nav-bttn-line" id="nav-bttn-line-two"></div>
     <div class="nav-bttn-line" id="nav-bttn-line-three"></div>
   </a>
- 
+
  <div id="sidebar-wrapper">
- 
+
                             <a href="<?php echo vibe_site_url(); ?>" class="logo"><img id="logo_img" src="<?php  echo apply_filters('wplms_logo_url',VIBE_URL.'/images/logo.png'); ?>" alt="<?php echo get_bloginfo('name'); ?>" />
                             <img src="<?php  $logo = vibe_get_option('logo_fixed');
                                 if(isset($logo) && $logo)
@@ -30,7 +30,7 @@
                                  'walker'          => new vibe_walker,
                                  'fallback_cb'     => 'vibe_set_menu'
                              ));
-                            wp_nav_menu( $args ); 
+                            wp_nav_menu( $args );
                         ?>
 
 
@@ -50,39 +50,35 @@
 							<div class="flags">
 							<?php echo do_shortcode('[gtranslate]'); ?>
 							</div>
-							
-							<div class="clear-xs"></div>
-							
-<a class="button donate" href="https://www.patreon.com/artprof" target="_blank">DONATE</a>
-<?php
-if ( function_exists('bp_loggedin_user_id') && bp_loggedin_user_id() ) {	?>			
-	<a class="button user" href="<?php echo bp_loggedin_user_domain(); ?>" target="_blank">MY PROFILE</a>	
-<?php } else { ?>
-	<a class="button user" href="/login/">LOG IN</a>		
-<?php }?>
 
 							<div class="clear-xs"></div>
-							<div class="socialmedia">
-								<div class="row">
-									<a href="https://www.instagram.com/art.prof/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-									<a href="https://www.facebook.com/claralieu/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-									<a href="https://www.pinterest.com/claralieu/" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-								</div>
-								<div class="row">
-									<a href="https://www.youtube.com/channel/UCG19ZyhUNbkPzU105yq8Rgw/featured" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-									<a href="https://twitter.com/claralieu" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-								</div>
-							</div>
-							
-							<div class="clear-xs"></div>
-								<div class="sitecredits">
-							&copy; <?php echo date('Y'); ?> ArtProf. All rights reserved.<br /><a href="/terms-and-conditions">Site Disclaimer</a>. Art Prof is not supported, endorsed, or sponsored by RISD.<br />.
-						
-							</div>
-							
-							
-							
-							
-				        </div>
-						
-						
+
+<a class="button donate" href="https://www.patreon.com/artprof" target="_blank">DONATE</a>
+<?php
+if ( function_exists('bp_loggedin_user_id') && bp_loggedin_user_id() ) {	?>
+	<a class="button user" href="<?php echo bp_loggedin_user_domain(); ?>" target="_blank">MY PROFILE</a>
+<?php } else { ?>
+	<a class="button user" href="/login/">LOG IN</a>
+<?php }?>
+
+<div class="clear-xs"></div>
+<div class="socialmedia">
+	<div class="row">
+		<a href="https://www.instagram.com/art.prof/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+		<a href="https://www.facebook.com/claralieu/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+		<a href="https://www.pinterest.com/claralieu/" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+	</div>
+	<div class="row">
+		<a href="https://www.youtube.com/channel/UCG19ZyhUNbkPzU105yq8Rgw/featured" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+		<a href="https://twitter.com/claralieu" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+	</div>
+</div>
+
+<div class="clear-xs"></div>
+	<div class="sitecredits">
+		&copy; <?php echo date('Y'); ?> ArtProf. All rights reserved.<br />
+    <a href="/terms-and-conditions">Site Disclaimer</a>.<br />
+    Art Prof is not supported, endorsed, or sponsored by RISD.<br />
+    Digital Partner: <a href="https://cantina.co/" target="_blank">Cantina.co</a><br />
+	</div>
+</div>
